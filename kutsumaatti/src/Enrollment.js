cd4624bbe747e9a2ca8fff625465c5614f18c255
Vesa Laakso <vesa.laakso@venuu.fi>
2016-08-11 23:41:30 +0300

    Edit enrollments

--- kutsumaatti/src/Enrollment.js
+++ kutsumaatti/src/Enrollment.js
@@ -2,21 +2,68 @@ import React, { Component } from 'react';
 import './Enrollment.css';
 
 class Enrollment extends Component {
-  render() {
-    const enrollee = this.props.enrollee;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false
+    }
+  }
 
-    if (enrollee.invalid) {
-      return (
-        <tr className='Kisamaatti-Enrollment Kisamaatti-Enrollment--invalid'>
-          <td colSpan='4'>
-            {enrollee.raw}
-          </td>
-        </tr>
-      )
+  render() {
+    if (this.state.isEditing) {
+      return this._renderEditRow();
     }
 
+    if (this.props.enrollee.invalid) {
+      return this._renderInvalidRow();
+    }
+
+    return this._renderViewRow();
+  }
+
+  _renderEditRow() {
+    const enrollee = this.props.enrollee;
+
     return (
-      <tr className='Kisamaatti-Enrollment'>
+      <tr className='Kisamaatti-Enrollment Kisamaatti-Enrollment--editing'>
+        <td colSpan='4'>
+          <input
+            type='text'
+            value={enrollee.raw}
+            ref={(node) => {
+              if (node) node.focus();
+            }}
+            onChange={ (evt) => this.props.onChange(evt.target.value) }
+            onBlur={ () => this.setState({ isEditing: false }) }
+          />
+        </td>
+      </tr>
+    )
+  }
+
+  _renderInvalidRow() {
+    const enrollee = this.props.enrollee;
+
+    return (
+      <tr
+        className='Kisamaatti-Enrollment Kisamaatti-Enrollment--invalid'
+        onClick={ () => this.setState({ isEditing: true }) }
+      >
+        <td colSpan='4'>
+          {enrollee.raw}
+        </td>
+      </tr>
+    )
+  }
+
+  _renderViewRow() {
+    const enrollee = this.props.enrollee;
+
+    return (
+      <tr
+        className='Kisamaatti-Enrollment'
+        onClick={ () => this.setState({ isEditing: true }) }
+      >
         <td>{enrollee.owner}</td>
         <td>{enrollee.vrl}</td>
         <td>{enrollee.horseName}</td>
