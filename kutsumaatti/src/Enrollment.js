7d03af89a4eb6f3fae4227247df7edd1f8944e85
Vesa Laakso <vesa.laakso@venuu.fi>
2016-08-11 23:20:32 +0300

    Convert Enrollment to a class

--- kutsumaatti/src/Enrollment.js
+++ kutsumaatti/src/Enrollment.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Enrollment.css';
 
-function Enrollment(props) {
-  const enrollee = props.enrollee;
+class Enrollment extends Component {
+  render() {
+    const enrollee = this.props.enrollee;
 
-  if (enrollee.invalid) {
+    if (enrollee.invalid) {
+      return (
+        <tr className='Kisamaatti-Enrollment Kisamaatti-Enrollment--invalid'>
+          <td colSpan='4'>
+            {enrollee.raw}
+          </td>
+        </tr>
+      )
+    }
+
     return (
-      <tr className='Kisamaatti-Enrollment Kisamaatti-Enrollment--invalid'>
-        <td colSpan='4'>
-          {enrollee.raw}
-        </td>
+      <tr className='Kisamaatti-Enrollment'>
+        <td>{enrollee.owner}</td>
+        <td>{enrollee.vrl}</td>
+        <td>{enrollee.horseName}</td>
+        <td>{enrollee.vh}</td>
       </tr>
     )
   }
-
-  return (
-    <tr className='Kisamaatti-Enrollment'>
-      <td>{enrollee.owner}</td>
-      <td>{enrollee.vrl}</td>
-      <td>{enrollee.horseName}</td>
-      <td>{enrollee.vh}</td>
-    </tr>
-  )
 }
 
 export default Enrollment;
